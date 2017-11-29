768a300e6983935f224e230eca40dc700d395243
Lazhari <lazhari.mohammed@outlook.com>
2017-11-29 16:44:27 +0000

    Create repo card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppHeaderComponent } from './app-header/app-header.component';
 
 import { GithubService } from './github.service';
 import { UserCardComponent } from './user-card/user-card.component';
+import { RepoCardComponent } from './repo-card/repo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
     AppHeaderComponent,
-    UserCardComponent
+    UserCardComponent,
+    RepoCardComponent
   ],
   imports: [
     BrowserModule,
