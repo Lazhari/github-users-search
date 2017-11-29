7b982e9889f1a98d7b13879fd10e69abedd6f77e
Lazhari <lazhari.mohammed@outlook.com>
2017-11-29 11:09:03 +0000

    Update the user component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'rxjs/add/operator/map';
+import { GithubService } from '../github.service';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  user: any;
+  repos: any;
+  username: string = 'lazhari';
 
-  constructor() { }
+  constructor(private _githubService: GithubService) {
+    this.user = null;
+  }
 
   ngOnInit() {
+    this.searchUser();
+  }
+  searchUser() {
+    this._githubService.updateUser(this.username);
+    // Get User Profile 
+    this._githubService.getUser().subscribe(user => {
+      this.user = user;
+    });
+    // Get user Repos 
+    this._githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+    });
   }
 
 }
