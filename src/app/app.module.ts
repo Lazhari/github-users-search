5b37e1059354595ab5a6ad4eb9a35cd290ddef6d
Lazhari <lazhari.mohammed@outlook.com>
2017-11-29 16:41:03 +0000

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 
 import { GithubService } from './github.service';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
